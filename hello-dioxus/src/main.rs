9313974efb91cca84f2f706d3c26119ff3a39dfb
Placeless <mikoprpr@hotmail.com>
2024-04-14 03:04:51 +0900

    add use

--- hello-dioxus/src/main.rs
+++ hello-dioxus/src/main.rs
@@ -1,9 +1,10 @@
 use dioxus::prelude::*;
 use dioxus::launch;
 use dioxus_web::launch::launch;
+use std::thread::Scope;
 
 fn main() {
-    dioxus_web::launch(app);
+    launch(app);
 }
 
 fn app(cx: Scope) -> Element {
