7aebd3a9f201bc1f6a6ca9e78bbbdf3e365b8df6
Placeless <mikoprpr@hotmail.com>
2024-04-14 03:07:29 +0900

    古いバージョンで試してみる……

--- hello-dioxus/src/main.rs
+++ hello-dioxus/src/main.rs
@@ -1,10 +1,7 @@
 use dioxus::prelude::*;
-use dioxus::launch;
-use dioxus_web::launch::launch;
-use std::thread::Scope;
 
 fn main() {
-    launch(app);
+    dioxus_web::launch(app);
 }
 
 fn app(cx: Scope) -> Element {
